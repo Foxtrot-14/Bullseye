7bdd871125eb90d304634f8e42637c56ee6fdcf2
Foxtrot-14 <noamanahmedai@gmail.com>
2024-05-16 10:41:42 +0530

    all-pages

--- UI/src/pages/Detail.tsx
+++ UI/src/pages/Detail.tsx
@@ -1,11 +1,64 @@
 import React from "react";
-import "./Detail.css"
+import "./Detail.css";
 const Detail: React.FC = () => {
+  const searchResults = {
+    "1. symbol": "AAPL",
+    "2. name": "Apple Inc.",
+    "3. type": "Equity",
+    "4. region": "United States",
+    "5. marketOpen": "09:30",
+    "6. marketClose": "16:00",
+    "7. timezone": "UTC-04",
+    "8. currency": "USD",
+    "9. matchScore": "1.0000",
+  };
+  const {
+    "1. symbol": symbol,
+    "2. name": name,
+    "3. type": type,
+    "4. region": region,
+    "5. marketOpen": marketOpen,
+    "6. marketClose": marketClose,
+    "7. timezone": timezone,
+    "8. currency": currency,
+    "9. matchScore": matchScore,
+  } = searchResults || {};
+  const formattedMarketHours = `${marketOpen} - ${marketClose} ${
+    timezone || "Unknown Timezone"
+  }`;
+
   return (
     <main>
       <article className="dmain">
         <section className="dcon">
-          <h1>Test</h1>
+          <h2 className="dquick">
+            <span className="quick">Name: </span>
+            {name}
+          </h2>
+          <h2 className="dquick">
+            <span className="quick">Symbol: </span>
+            {symbol}
+          </h2>
+          <h2 className="dquick">
+            <span className="quick">Type: </span>
+            {type}
+          </h2>
+          <h2 className="dquick">
+            <span className="quick">Region: </span>
+            {region}
+          </h2>
+          <h2 className="dquick">
+            <span className="quick">Market hours: </span>
+            {formattedMarketHours}
+          </h2>
+          <h2 className="dquick">
+            <span className="quick">Currency hours: </span>
+            {currency}
+          </h2>
+          <h2 className="dquick">
+            <span className="quick">Match Score: </span>
+            {matchScore}
+          </h2>
         </section>
       </article>
     </main>
